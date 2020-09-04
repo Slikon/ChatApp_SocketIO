86bd4b295edb13c88f0c95a910a2b1334a4c8f9f
Slikon <slava.kulik27@gmail.com>
2020-09-05 02:04:45 +0300

    added: timestamps for every event in a chat

--- script.js
+++ script.js
@@ -3,29 +3,35 @@ const messageForm = document.getElementById('send-container')
 const messageContainer = document.getElementById('message-container')
 const messageInput = document.getElementById('message-input')
 
+//Function returns the current time at the moment when it is called
+function getDate(){
+    const date = new Date()
+    return `[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`
+}
+
 //User connecting to the room and entering its name
 const name = prompt('What is your name?')
-appendMessage('You joined!')
+appendMessage(`${getDate()} You joined!`)
 socket.emit('new-user', name)
 
 //Below is the list of events that client socket recieves from the server to react appropriately
 socket.on('chat-message', data => {
-    appendMessage(`${data.name}: ${data.message}`)
+    appendMessage(`${getDate()} ${data.name}: ${data.message}`)
 })
 
 socket.on('user-connected', name => {
-    appendMessage(`User ${name} connected`)
+    appendMessage(`${getDate()} User ${name} connected`)
 })
 
 socket.on('user-disconnected', name => {
-    appendMessage(`User ${name} disconnected`)
+    appendMessage(`${getDate()} User ${name} disconnected`)
 })
 
 //Submit button click
 messageForm.addEventListener('submit', element => {
     element.preventDefault()
     const message = messageInput.value
-    appendMessage(`You: ${message}`)
+    appendMessage(`${getDate()} You: ${message}`)
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
